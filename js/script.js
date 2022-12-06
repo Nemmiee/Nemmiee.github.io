e1591b3108a8646372fb6e061ccb0124c46dde93
Hoai <nthnam.a1.c3tqcap@gmail.com>
2022-12-06 20:44:35 +0700

    12/6/2022

--- js/script.js
+++ js/script.js
@@ -5,20 +5,20 @@
 /* #----------------# */
 
 /* Hiển thị nội dung của nút tìm kiếm ở phàn Header */
-let kiemdich=1;
-function search_dropdown(){
-    if(kiemdich===1){
-        document.querySelector("#header-search .header-dropdown").style.display="block";
-        kiemdich=0;
-    }else{
-        document.querySelector("#header-search .header-dropdown").style.display="none";
-        kiemdich=1;
+let kiemdich = 1;
+function search_dropdown() {
+    if (kiemdich === 1) {
+        document.querySelector("#header-search .header-dropdown").style.display = "block";
+        kiemdich = 0;
+    } else {
+        document.querySelector("#header-search .header-dropdown").style.display = "none";
+        kiemdich = 1;
     }
 }
 
 /* Hiển thị Menu 3 gạch khi responsive */
 let showSidebar = document.getElementById("hamburger-icon");
-showSidebar.addEventListener("click", function() {
+showSidebar.addEventListener("click", function () {
     document.getElementById("sidebar-container").classList.remove("hide");
     document.getElementById("sidebar-container").classList.add("hienThi");
 });
@@ -80,3 +80,92 @@ window.addEventListener("scroll", function () {
         fixedHeader.classList.remove("fixed");
     }
 });
+
+
+let isDarkMode = 1;
+let darkModeBtn = document.getElementById("darkmode-btn");
+darkModeBtn.addEventListener('click', function () {
+    if (isDarkMode == 1) {
+        isDarkMode = 0;
+        changeWhiteMode();
+    }
+    else {
+        isDarkMode = 1;
+        changeDarkMode();
+    }
+});
+
+function changeDarkMode() {
+    let pos = window.location.pathname.lastIndexOf('/');
+    let path = window.location.pathname.substring(pos + 1);
+    path = path.split(".")[0];
+    //document.getElementById("darkmode-i").style.color = "white";
+    //document.getElementById("header").style.backgroundColor = "black";
+    // for (var i = 0; i < document.querySelectorAll('.header-title').length; i++) {
+    //     document.querySelectorAll('.header-title')[i].classList.remove("darkmode");
+    // }
+    // for (var i = 0; i < document.querySelectorAll('.header-icon').length; ++i) {
+    //     document.querySelectorAll('.header-icon')[i].classList.remove("darkmode");
+    // }
+    // Footer
+    //document.getElementById("footer").style.backgroundColor = "#222222";
+    //document.getElementById("footer").classList.remove("darkmode");
+
+    // Content
+    if (path === "index") {
+        document.getElementById("content").classList.remove("darkmode");
+        document.getElementById("top-shopnow-container").classList.remove("darkmode");
+        document.getElementById("bottom-shopnow-container").classList.remove("darkmode");
+        document.getElementById("main-content").classList.remove("darkmode");
+    }
+    else if (path === "products") {
+
+    }
+    else if (path === "sizecheck") {
+
+    }
+    else if (path === "contact") {
+
+    }
+    else {
+
+    }
+}
+
+function changeWhiteMode() {
+    let pos = window.location.pathname.lastIndexOf('/');
+    let path = window.location.pathname.substring(pos + 1);
+    path = path.split(".")[0];
+    // Header
+    //document.getElementById("darkmode-i").style.color = "black";
+    //document.getElementById("header").style.backgroundColor = "white";
+    // for (var i = 0; i < document.querySelectorAll('.header-title').length; i++) {
+    //     document.querySelectorAll('.header-title')[i].classList.add("darkmode");
+    // }
+    // for (var i = 0; i < document.querySelectorAll('.header-icon').length; ++i) {
+    //     document.querySelectorAll('.header-icon')[i].classList.add("darkmode");
+    // }
+    // Footer
+    //document.getElementById("footer").style.backgroundColor = "white";
+    //document.getElementById("footer").classList.add("darkmode");
+
+    // Content
+    if (path === "index") {
+        document.getElementById("content").classList.add("darkmode");
+        document.getElementById("top-shopnow-container").classList.add("darkmode");
+        document.getElementById("bottom-shopnow-container").classList.add("darkmode");
+        document.getElementById("main-content").classList.add("darkmode");
+    }
+    else if (path === "products") {
+
+    }
+    else if (path === "sizecheck") {
+
+    }
+    else if (path === "contact") {
+
+    }
+    else {
+
+    }
+}
